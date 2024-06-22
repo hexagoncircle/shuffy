6addb75ccd234287402400e5c2d80d1224e3f172
Ryan Mulligan <hexagoncircle@gmail.com>
2024-06-22 07:07:50 -0700

    Set up project config

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@css": path.resolve(__dirname, "./src/css"),
+      "@js": path.resolve(__dirname, "./src/js"),
+    },
+  },
+  plugins: [react(), svgr()],
+});
