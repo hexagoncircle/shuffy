a0567fbead31143e9bf2c62629c871cc2d93e1a1
Ryan Mulligan <hexagoncircle@gmail.com>
2024-07-01 12:21:50 -0700

    Add style updates

--- src/components/IntroScreen.tsx
+++ src/components/IntroScreen.tsx
@@ -12,12 +12,12 @@ export default function IntroScreen() {
   const ref = useRef(null);
 
   useGSAP(() => {
-    const tl = gsap.timeline({ repeat: -1, repeatDelay: 2, ease: "elastic.out(1,0.3)" });
+    const tl = gsap.timeline();
     const ease = "expo.out";
     const elasticEase = "elastic.out(0.15,0.12)";
     const duration = 0.5;
-    const pull = 40;
-    const rotation = 11;
+    const sidePull = 12;
+    const sideRotation = 11;
 
     tl
       .set("[data-animate=scene]", {
@@ -34,17 +34,21 @@ export default function IntroScreen() {
       })
       .set("[data-animate=letter-group]", {
         opacity: 0,
-        yPercent: -50
+        yPercent: -50,
       })
       .set("[data-animate=letter]", {
         xPercent: 11,
         yPercent: 4,
       })
+      .set("[data-animate=tagline]", {
+        opacity: 0,
+        scale: 0.95
+      })
       .to("[data-animate=scene]", {
         scale: 1,
         yPercent: -10,
         duration,
-        delay: 1,
+        delay: 0.5,
         ease
       })
       .to("[data-animate=logo]", {
@@ -60,17 +64,16 @@ export default function IntroScreen() {
         stagger: 0.04
       }, "<")
       .to("[data-animate=letter-group]", {
-        xPercent: 0,
         yPercent: 0,
         duration,
         ease,
-        stagger: 0.03
+        stagger: 0.04
       }, "<")
       .to("[data-animate=letter]", {
         xPercent: 0,
         yPercent: 0,
         duration,
-        ease: "back.out(4)",
+        ease: "back.out(5)",
         stagger: 0.04
       }, "<+=25%")
       .to("[data-animate=cards]", {
@@ -87,30 +90,39 @@ export default function IntroScreen() {
         duration: duration * 2,
         ease: elasticEase,
       }, "<+=10%")
+      .fromTo("[data-animate=main]", {
+        rotate: 20,
+        scale: 0.2
+      }, {
+        rotate: 0,
+        scale: 1,
+        duration: duration * 1.25,
+        ease,
+      }, "<+=20%")
       .to("[data-animate=face]", {
         yPercent: -15,
-        duration: duration,
+        duration,
         ease: "power2.in",
       }, "<-=30%")
       .to("[data-animate=face]", {
         yPercent: 0,
-        duration: duration,
+        duration,
         ease: "back.out(3)",
       }, ">")
       .to("[data-animate=cheeks]", {
         yPercent: -35,
-        duration: duration,
+        duration,
         ease: "power2.in",
       }, "<-=90%")
       .to("[data-animate=cheeks]", {
         yPercent: 0,
-        duration: duration,
+        duration,
         ease: "back.out(3)",
       }, ">")
       .to("[data-animate=nose]", {
         transformOrigin: "60% left",
         rotate: -15,
-        duration: duration,
+        duration,
         ease: "power4.in",
       }, "<-=95%")
       .to("[data-animate=nose]", {
@@ -122,21 +134,27 @@ export default function IntroScreen() {
         yPercent: 100
       }, {
         yPercent: 40,
-        x: (i) => i === 0 ? pull : -pull,
+        xPercent: (i) => i === 0 ? sidePull : -sidePull,
         duration: duration * 1.5,
         ease: elasticEase,
         stagger: 0.08
-      }, "<-=25%")
+      }, "<-=30%")
       .to("[data-animate=side]", {
-        rotate: (i) => i === 0 ? rotation : -rotation,
-        duration: duration * 2,
+        rotate: (i) => i === 0 ? sideRotation : -sideRotation,
+        duration: duration * 2.5,
         ease,
       }, "<+=10%")
+      .to("[data-animate=tagline]", {
+        opacity: 1,
+        scale: 1,
+        duration,
+        ease,
+      }, "<+=70%")
 
   }, { scope: ref })
 
   return (
-    <article ref={ref} className={clsx(styles.intro, "flow")}>
+    <article ref={ref} className={styles.intro}>
       <figure className={clsx(styles.scene, "stack")} data-animate="scene">
         <div className={clsx(styles.cards, "stack")} data-animate="cards">
           <img className={styles.left} data-animate="side" src="card-alt.svg" />
@@ -145,7 +163,7 @@ export default function IntroScreen() {
         </div>
         <Logo className={styles.logo} data-animate="logo" />
       </figure>
-      {/* <p className="font-display">a choice of chance for shruggie moods</p> */}
+      <p className={clsx(styles.tagline, "font-display")} data-animate="tagline">a choice of chance for shruggie moods</p>
     </article>
   )
 }
