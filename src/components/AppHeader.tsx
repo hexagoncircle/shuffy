5d5043274eecb7886fd4af6a7fee772cb5550c34
Ryan Mulligan <hexagoncircle@gmail.com>
2024-08-08 05:23:20 -0700

    Move settings modal into component

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -11,58 +11,22 @@ import { Link } from "react-router-dom";
 import Switch from "./Switch";
 import { CategoriesContext } from "./CategoriesContext";
 import { ConfirmModalContext } from "./ConfirmModalContext";
+import SettingsModal from "./SettingsModal";
 
 export default function AppHeader() {
   const {
     deckName,
-    isSettingsActive,
-    repeatCard,
-    shuffleAnimation,
-    setDeckName,
     setIsSettingsActive,
-    setRepeatCard,
-    setShuffleAnimation
   } = useContext(SettingsContext);
-  const { cards, deleteAllCards } = useContext(CardsContext);
-  const { categories, deleteAllCategories } = useContext(CategoriesContext);
-  const { setModalContext } = useContext(ConfirmModalContext);
+  const { cards } = useContext(CardsContext);
+  const { categories } = useContext(CategoriesContext);
   const [hasNotification, setHasNotification] = useState(!categories.length);
   const cardCount = cards.length;
   const deckNameDisplayText = deckName || "¯\\_(ツ)_/¯";
 
-  const handleDeckNameChange = (e: SyntheticEvent<HTMLInputElement>) => {
-    setDeckName(e.currentTarget.value);
-  }
-
   const handleSettingsToggleClick = () => {
     setIsSettingsActive(true)
     if (hasNotification) setHasNotification(false);
-  }
-
-  const handleDeleteCardsClick = () => {
-    setModalContext({
-      isOpen: true,
-      title: "Are you sure?",
-      message: `This action removes all the cards from the deck. Once they are deleted, they are gone forever.`,
-      actionConfirmText: "Yes, reset the deck",
-      actionCancelText: "Cancel",
-      onConfirm: () => {
-        deleteAllCards()
-      }
-    })
-  }
-
-  const handleDeleteCategoriesClick = () => {
-    setModalContext({
-      isOpen: true,
-      title: "Are you sure?",
-      message: `This action removes all categories from the deck. Your cards are safe! However, they will no longer be assigned a category. You can still update your cards once new categories are created.`,
-      actionConfirmText: "Yes, delete all categories",
-      actionCancelText: "Cancel",
-      onConfirm: () => {
-        deleteAllCategories()
-      }
-    })
   }
 
   return (
@@ -81,64 +45,7 @@ export default function AppHeader() {
       )}
 
       <SettingsToggle hasNotification={hasNotification} onClick={handleSettingsToggleClick} />
-
-      <Modal title="Deck Settings" variant="drawer" open={isSettingsActive} onClose={() => setIsSettingsActive(false)}>
-        <section className="app-header-modal-section flow">
-          <Categories />
-        </section>
-
-        <section className="app-header-modal-section flow">
-          <div>
-            <label htmlFor="input-with-label">Deck name</label>
-            <input
-              id="input-with-label"
-              type="text"
-              defaultValue={deckName}
-              autoComplete="off"
-              placeholder="Add a name"
-              onChange={handleDeckNameChange}
-            />
-          </div>
-          <div className="checkbox-wrapper">
-            <Switch id="card-active-toggle" variant="compact" checked={repeatCard} onChange={() => setRepeatCard(!repeatCard)} />
-            <label htmlFor="card-active-toggle">
-              Allow same card in back-to-back shuffies
-            </label>
-          </div>
-          <div className="checkbox-wrapper">
-            <Switch id="shuffle-animation" variant="compact" checked={shuffleAnimation} onChange={() => setShuffleAnimation(!shuffleAnimation)} />
-            <label htmlFor="shuffle-animation">
-              Play animation when shuffy-ing
-            </label>
-          </div>
-        </section>
-
-        <section
-          className="app-header-modal-section cluster"
-          style={{ "--justify": "flex-end" } as CSSProperties}
-        >
-          {categories.length ? (
-            <button
-              id="delete-categories"
-              className="danger"
-              type="button"
-              onClick={handleDeleteCategoriesClick}
-            >
-              Delete all categories
-            </button>
-          ) : null}
-          {cards.length ? (
-            <button
-              id="delete-cards"
-              className="danger"
-              type="button"
-              onClick={handleDeleteCardsClick}
-            >
-              Reset the deck
-            </button>
-          ) : null}
-        </section>
-      </Modal>
+      <SettingsModal />
     </header>
   )
 }
---
+++ src/components/SettingsModal.tsx
@@ -0,0 +1,115 @@
+import { CSSProperties, SyntheticEvent, useContext } from "react";
+import { SettingsContext } from "@components/SettingsContext";
+import { CardsContext } from "./CardsContext";
+import Categories from "./Categories";
+import Modal from "./Modal";
+import Switch from "./Switch";
+import { CategoriesContext } from "./CategoriesContext";
+import { ConfirmModalContext } from "./ConfirmModalContext";
+import "@css/settings.css";
+
+export default function SettingsModal() {
+  const {
+    deckName,
+    isSettingsActive,
+    repeatCard,
+    shuffleAnimation,
+    setDeckName,
+    setIsSettingsActive,
+    setRepeatCard,
+    setShuffleAnimation
+  } = useContext(SettingsContext);
+  const { cards, deleteAllCards } = useContext(CardsContext);
+  const { categories, deleteAllCategories } = useContext(CategoriesContext);
+  const { setModalContext } = useContext(ConfirmModalContext);
+
+  const handleDeckNameChange = (e: SyntheticEvent<HTMLInputElement>) => {
+    setDeckName(e.currentTarget.value);
+  }
+
+  const handleDeleteCardsClick = () => {
+    setModalContext({
+      isOpen: true,
+      title: "Are you sure?",
+      message: `This action removes all the cards from the deck. Once they are deleted, they are gone forever.`,
+      actionConfirmText: "Yes, reset the deck",
+      actionCancelText: "Cancel",
+      onConfirm: () => {
+        deleteAllCards()
+      }
+    })
+  }
+
+  const handleDeleteCategoriesClick = () => {
+    setModalContext({
+      isOpen: true,
+      title: "Are you sure?",
+      message: `This action removes all categories from the deck. Your cards are safe! However, they will no longer be assigned a category. You can still update your cards once new categories are created.`,
+      actionConfirmText: "Yes, delete all categories",
+      actionCancelText: "Cancel",
+      onConfirm: () => {
+        deleteAllCategories()
+      }
+    })
+  }
+
+  return (
+    <Modal title="Deck Settings" variant="drawer" open={isSettingsActive} onClose={() => setIsSettingsActive(false)}>
+      <section className="settings-section flow">
+        <Categories />
+      </section>
+
+      <section className="settings-section flow">
+        <div>
+          <label htmlFor="input-with-label">Deck name</label>
+          <input
+            id="input-with-label"
+            type="text"
+            defaultValue={deckName}
+            autoComplete="off"
+            placeholder="Add a name"
+            onChange={handleDeckNameChange}
+          />
+        </div>
+        <div className="checkbox-wrapper">
+          <Switch id="card-active-toggle" variant="compact" checked={repeatCard} onChange={() => setRepeatCard(!repeatCard)} />
+          <label htmlFor="card-active-toggle">
+            Allow same card in back-to-back shuffies
+          </label>
+        </div>
+        <div className="checkbox-wrapper">
+          <Switch id="shuffle-animation" variant="compact" checked={shuffleAnimation} onChange={() => setShuffleAnimation(!shuffleAnimation)} />
+          <label htmlFor="shuffle-animation">
+            Play animation when shuffy-ing
+          </label>
+        </div>
+      </section>
+
+      <section
+        className="settings-section cluster"
+        style={{ "--justify": "flex-end" } as CSSProperties}
+      >
+        {categories.length ? (
+          <button
+            id="delete-categories"
+            className="danger"
+            type="button"
+            onClick={handleDeleteCategoriesClick}
+          >
+            Delete all categories
+          </button>
+        ) : null}
+        {cards.length ? (
+          <button
+            id="delete-cards"
+            className="danger"
+            type="button"
+            onClick={handleDeleteCardsClick}
+          >
+            Reset the deck
+          </button>
+        ) : null}
+      </section>
+    </Modal>
+  )
+}
