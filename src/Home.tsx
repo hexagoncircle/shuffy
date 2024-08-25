b7bdb4c088bba2db16cefcb6dfbac56a50a80f2c
Ryan Mulligan <hexagoncircle@gmail.com>
2024-08-25 09:54:23 -0700

    Remove unnecessary useEffect

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,12 +1,7 @@
-import { useEffect } from "react";
 import IntroScreen from "./components/IntroScreen";
 import styles from "@css/Home.module.css"
 
 export default function Home() {
-  useEffect(() => {
-    document.body.classList.add("vibes")
-  }, []);
-
   return (
     <main className={styles.scene}>
       <h1 className="visually-hidden">Shuffy</h1>
